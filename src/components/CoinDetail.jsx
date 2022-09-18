914970dab08869589f5229182bf8c75327443ab6
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-09-18 22:17:29 +0530

    chart for coins

--- src/components/CoinDetail.jsx
+++ src/components/CoinDetail.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useAxios from '../hooks/useAxios'
 
 const CoinDetail = () => {
+    const {id} = useParams();
+    const {response} = useAxios(`coins/${id}?localization=false&tickers=false&market_data=false&community_data=false&sparkline=false`)
+
+    if(!response)
+    {
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+    }
   return (
-    <div>CoinDetail</div>
+    <div className='my-6'>
+        <div className='flex gap-2 items-center'>
+            <img src={response.image.small} alt={response.name}/>
+            <h1 className='text-2xl mb-2 capitalize font-bold'>
+                {response.name}
+            </h1>
+            <p className='mt-6 text-gray-500 [&>a]:text-blue-600 [&>a]:underline' dangerouslySetInnerHTML = {{__html: response.description.en}}></p>
+        </div>
+    </div>
   )
 }
 
